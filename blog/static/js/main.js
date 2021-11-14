479ece56cb0791822902dd18beb6b97e015e9646
cheyfair <cheylfair@gmail.com>
2021-11-14 17:41:24 +0000

    New JavaScript and CSS.

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -63,5 +63,25 @@ for (i =0; i < skillsHeader.length; i++) {
     });
 }
 
+/*TABS QUALIFICATION*/
+const tabs = document.querySelectorAll('[data-target]');
+const tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tab.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
+
 /*SCROLL WORK*/
 /*SCROLL CONTACT*/
